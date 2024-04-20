f90ec72674c5bbbf0ed22605701e6e9bfcfdaa71
Den <serge23023@gmial.com>
2024-04-20 10:54:08 +0300

    edit property

--- src/category.py
+++ src/category.py
@@ -2,22 +2,23 @@ from src.product import Product
 
 
 class Category:
-    _total_categories = 0
-    _total_unique_products = 0
+    __total_categories = 0
+    __total_unique_products = 0
 
     def __init__(self, name: str, description: str, products: list[dict]):
-        self._name = name
-        self._description = description
+        self.__name = name
+        self.__description = description
         self._products = self.init_products(products)
-        Category._total_categories += 1
+        Category.__total_categories += 1
+
 
     @classmethod
     def total_categories(cls):
-        return cls._total_categories
+        return cls.__total_categories
 
     @classmethod
     def total_unique_products(cls):
-        return cls._total_unique_products
+        return cls.__total_unique_products
 
     @staticmethod
     def init_products(products: list[dict]) -> list[Product]:
@@ -26,8 +27,11 @@ class Category:
         for product in products:
             list_products.append(Product(**product))
             set_name.add(product['name'])
-        Category._total_unique_products += len(set_name)
+        Category.__total_unique_products += len(set_name)
         return list_products
+
+    def __repr__(self):
+        return f"name: {self.__name}, description: {self.__description}, products: {self._products}"
 
     # def add_product(self, product: dict):
     #     self.products.append(Product(**product))
