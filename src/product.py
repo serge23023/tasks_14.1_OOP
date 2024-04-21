0a3efea0d3d81fc42e1bb88a8eff014f9ccc8f81
Den <serge23023@gmial.com>
2024-04-21 13:32:15 +0300

    add create_product

--- src/product.py
+++ src/product.py
@@ -1,4 +1,25 @@
 class Product:
+
+    @classmethod
+    def create_product(cls, new_product: dict, product_list: list = None):
+        if product_list is None:
+            product_list = []
+
+        for product in product_list:
+            if product.name == new_product['name']:
+                # Найден дубликат товара
+                if product.price >= new_product['price']:
+                    # Если у существующего товара цена выше, используем его цену
+                    product.quantity += new_product['quantity']
+                    # product_list.remove(product)
+                    return
+                else:
+                    # Иначе используем новую цену
+                    product.price = new_product['price']
+                    product.quantity += new_product['quantity']
+                    return
+        return cls(**new_product)
+
     def __init__(self, name: str, description: str, price: float, quantity: int):
         self.name = name
         self.description = description
