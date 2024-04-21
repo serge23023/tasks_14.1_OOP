41cfdf2970bb07106e63fcf284f3697d8ca80e50
Den <serge23023@gmial.com>
2024-04-21 13:33:10 +0300

    edit utils.py

--- src/utils.py
+++ src/utils.py
@@ -6,12 +6,12 @@ from src.read_file import open_json
 def create_categories():
     categories = []
     for item in open_json('products.json'):
-        item['products'] = [Product(**product) for product in item['products']]
+        item['products'] = [Product.create_product(product) for product in item['products']]
         categories.append(Category(**item))
     return categories
 
 
-def add_property_to_category(categories: list[Category], name: str, product: dict):
+def add_product_to_category(categories: list[Category], name: str, product: dict):
     for category in categories:
         if category.name == name:
-            category.add_product(Product(**product))
+            category.add_product(product)
