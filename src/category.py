cbfef27d5946f35bfbd334ebfce0a9e45947bc8e
Den <serge23023@gmial.com>
2024-04-21 13:30:55 +0300

    edit category

--- src/category.py
+++ src/category.py
@@ -13,12 +13,17 @@ class Category:
     def total_unique_products(cls):
         return cls.__total_unique_products
 
+    @classmethod
+    def reset(cls):
+        cls.__total_categories = 0
+        cls.__total_unique_products = 0
+
     def __init__(self, name: str, description: str, products: list[Product] = None):
         self.__name = name
         self.__description = description
-        self.__products = products if (products is not None) else []
+        self.__products = products if products else []
         Category.__total_categories += 1
-        Category.__total_unique_products += len(set(self.products)) if self.products else 0
+        Category.__total_unique_products += len(set(p.name for p in self.__products))
 
     @property
     def products(self):
@@ -29,9 +34,10 @@ class Category:
         return self.__name
 
     def __repr__(self):
-        return f"\nname: {self.__name}, description: {self.__description}, products: {self.products}"
+        return f"\nname: {self.__name}, description: {self.__description}, products: {self.__products}"
 
-    def add_product(self, product: Product):
-        if product.name not in set(p.name for p in self.products):
+    def add_product(self, product: dict):
+        new_product = Product.create_product(product, self.__products)
+        if product['name'] not in (p.name for p in self.__products):
             Category.__total_unique_products += 1
-        self.products.append(product)
+            self.__products.append(new_product)
