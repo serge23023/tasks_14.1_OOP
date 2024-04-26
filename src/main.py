f46aeb3346f2cb1d0dadcd1461764996f77a171f
Den <serge23023@gmial.com>
2024-04-27 01:25:24 +0300

    edit

--- src/main.py
+++ src/main.py
@@ -7,7 +7,3 @@ if __name__ == '__main__':
     categories = create_categories()
     for category in categories:
         print(category)
-    # categories = create_categories()
-    # add_property_to_category(categories, 'Смартфоны',
-    #                          {'name': 'name', 'description': 'description', 'price': 0.0, 'quantity': 0})
-    # print([p for p in categories])
