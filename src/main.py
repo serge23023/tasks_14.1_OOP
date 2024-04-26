e395a9b8baaa3d6f2b9525fa4c515f53820acc50
Den <serge23023@gmial.com>
2024-04-27 01:55:24 +0300

    edit

--- src/main.py
+++ src/main.py
@@ -2,8 +2,14 @@ from src.for_in_categories import ForInCategories
 from src.utils import create_categories
 
 if __name__ == '__main__':
-    for product in ForInCategories(create_categories()[0].products):
-        print(product)
     categories = create_categories()
+
+    for product in ForInCategories(categories[0].products):
+        print(product)
+
+    print('---------------')
     for category in categories:
         print(category)
+
+    print('---------------')
+    print(categories[0].products[0] + categories[0].products[1])
