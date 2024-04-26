d58030a053926f0ae1af672b49e6995d9c6e4b79
Den <serge23023@gmial.com>
2024-04-27 01:07:53 +0300

    edit

--- src/main.py
+++ src/main.py
@@ -1,7 +1,10 @@
 # from src.utils import add_property_to_category
+from src.for_in_categories import ForInCategories
 from src.utils import create_categories
 
 if __name__ == '__main__':
+    for product in ForInCategories(create_categories()[0].products):
+        print(product)
     categories = create_categories()
     for category in categories:
         print(category)
