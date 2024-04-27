c85f92f912a8524d7d1895b05f0013e432ab7cda
Den <serge23023@gmial.com>
2024-04-27 23:20:40 +0300

    add class Smartphone

---
+++ src/classes/smartphone.py
@@ -0,0 +1,10 @@
+from src.classes.product import Product
+
+
+class Smartphone(Product):
+    def __init__(self, name, description, price, quantity, performance, model, memory, color):
+        super().__init__(name, description, price, quantity)
+        self.performance = performance
+        self.model = model
+        self.memory = memory
+        self.color = color
